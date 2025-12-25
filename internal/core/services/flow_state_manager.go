c184626500fb3fad12fb6880030e2209a7758adb
Luiz Ricardo <luizrgfelipe@gmail.com>
2025-12-25 17:45:38 -0300

    🛠  REFACTOR: change to pointer

--- internal/core/services/flow_state_manager.go
+++ internal/core/services/flow_state_manager.go
@@ -3,5 +3,5 @@ package services_interfaces
 import "github.com/luizrgf2/universal-flow/internal/core/entities"
 
 type FlowStateManagerService interface {
-	RunNewFlow(flow entities.Flow) error
+	RunNewFlow(flow *entities.Flow) error
 }
