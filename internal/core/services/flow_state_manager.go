a5a1b9acc02f91465b4be0c44bf7d1b9656b19be
Luiz Ricardo <luizrgfelipe@gmail.com>
2025-12-25 19:05:27 -0300

    🛠  REFACTOR: add new methods to interface

--- internal/core/services/flow_state_manager.go
+++ internal/core/services/flow_state_manager.go
@@ -3,5 +3,7 @@ package services_interfaces
 import "github.com/luizrgf2/universal-flow/internal/core/entities"
 
 type FlowStateManagerService interface {
-	RunNewFlow(flow *entities.Flow) error
+	CreateFlow(flow *entities.Flow) error
+	UpdateFlow(flow *entities.Flow) error
+	GetFlowState(flowId string) (*entities.Flow, error)
 }
