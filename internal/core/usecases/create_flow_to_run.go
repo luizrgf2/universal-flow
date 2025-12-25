45b2457d2b9bb704bd4fcf3029f887781f74cef9
Luiz Ricardo <luizrgfelipe@gmail.com>
2025-12-25 19:18:18 -0300

    📦 FEAT: add use case to create and run new flows

---
+++ internal/core/usecases/create_flow_to_run.go
@@ -0,0 +1,65 @@
+package usecases
+
+import (
+	"github.com/luizrgf2/universal-flow/internal/core/entities"
+	flowengine "github.com/luizrgf2/universal-flow/internal/core/flow_engine"
+	services_interfaces "github.com/luizrgf2/universal-flow/internal/core/services"
+)
+
+type CreateFlowUseCaseNodes struct {
+	ID         string   `json:"id"`
+	Name       string   `json:"name"`
+	ScriptPath string   `json:"script_path"`
+	OutputNode []string `json:"output_node"`
+}
+
+type CreateFlowUseCaseInput struct {
+	ID    string                   `json:"id"`
+	Name  string                   `json:"name"`
+	Nodes []CreateFlowUseCaseNodes `json:"nodes"`
+}
+
+type CreateFlowToRunUseCase struct {
+	flowStateManagerService services_interfaces.FlowStateManagerService
+}
+
+func (uc *CreateFlowToRunUseCase) createNodesWithInput(nodesInput *[]CreateFlowUseCaseNodes) (*[]entities.Node, error) {
+	nodes := []entities.Node{}
+
+	for _, node := range *nodesInput {
+		nodeToCreate, err := entities.CreateNode(node.ID, node.Name, node.ScriptPath, node.OutputNode)
+		if err != nil {
+			return nil, err
+		}
+		nodes = append(nodes, *nodeToCreate)
+	}
+	return &nodes, nil
+}
+
+func (uc *CreateFlowToRunUseCase) Execute(input CreateFlowUseCaseInput) error {
+
+	nodes, err := uc.createNodesWithInput(&input.Nodes)
+	if err != nil {
+		return err
+	}
+
+	flow, err := entities.CreateFlow(input.ID, input.Name, *nodes)
+	if err != nil {
+		return err
+	}
+
+	err = uc.flowStateManagerService.CreateFlow(flow)
+	if err != nil {
+		return err
+	}
+
+	flowengine := flowengine.NewFlowEngine(uc.flowStateManagerService)
+
+	err = flowengine.RunFlow(flow)
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
