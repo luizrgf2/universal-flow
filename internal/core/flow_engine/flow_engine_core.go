685e4c361dc45059c5d3e725f0ca8f2e501a939a
Luiz Ricardo <luizrgfelipe@gmail.com>
2025-12-25 16:38:41 -0300

    📦 FEAT: create flow engine core to manager flows

---
+++ internal/core/flow_engine/flow_engine_core.go
@@ -0,0 +1,140 @@
+package flowengine
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+
+	"github.com/luizrgf2/universal-flow/internal/core/entities"
+	services_interfaces "github.com/luizrgf2/universal-flow/internal/core/services"
+	"github.com/luizrgf2/universal-flow/internal/core/types"
+)
+
+type FlowEngineCore struct {
+	flowStateManagerService services_interfaces.FlowStateManagerService
+}
+
+func NewFlowEngine(flowStateManagerService services_interfaces.FlowStateManagerService) *FlowEngineCore {
+	return &FlowEngineCore{flowStateManagerService: flowStateManagerService}
+}
+
+func (fe *FlowEngineCore) getNodeById(id string, nodes []entities.Node) (*entities.Node, error) {
+	for _, node := range nodes {
+		if node.ID == id {
+			return &node, nil
+		}
+	}
+	return nil, fmt.Errorf("node with id: %s not exists in flow", id)
+}
+
+func (fe *FlowEngineCore) selectNextNodeToRun(flow *entities.Flow) (entities.Node, error) {
+	if flow.NextNode == nil {
+		return flow.Nodes[0], nil
+	} else {
+		node, err := fe.getNodeById(*flow.NextNode, flow.Nodes)
+		if err != nil {
+			return entities.Node{}, err
+		}
+		return *node, nil
+	}
+}
+
+func (fe *FlowEngineCore) updateFlowState(flow entities.Flow) error {
+	if flow.CurrentNode == nil && flow.NextNode == nil && flow.PreviousNode == nil {
+		err := fe.flowStateManagerService.RunNewFlow(flow)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	return nil
+}
+
+func (fe *FlowEngineCore) changeFlowStatus(flow entities.Flow, status string) error {
+	statusToChange, err := types.CreateFlowStatus(status)
+
+	if flow.Status == statusToChange {
+		return nil
+	}
+
+	err = flow.ChangeFlowStatus(statusToChange)
+	if err != nil {
+		return err
+	}
+
+	err = fe.updateFlowState(flow)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (fe *FlowEngineCore) execJSNodeOrBun(nodeToRun *entities.Node) error {
+	comands := strings.Split(nodeToRun.ScriptPath, " ")
+	comandMain := comands[0]
+	comands = comands[1:]
+
+	if (strings.Contains(nodeToRun.ScriptPath, "node") || strings.Contains(nodeToRun.ScriptPath, "bun")) && strings.Contains(nodeToRun.ScriptPath, ".js") {
+		execNode := exec.Command(comandMain, comands...)
+		result := execNode.Run()
+
+		failedStatus, err := types.CreateNodeStatus("failed")
+		if err != nil {
+			return err
+		}
+
+		if result != nil {
+			nodeToRun.ChangeNodeStatus(failedStatus)
+			nodeToRun.ChangeError(result.Error())
+			return result
+		}
+
+		completedStatus, err := types.CreateNodeStatus("completed")
+		if err != nil {
+			return err
+		}
+
+		nodeToRun.ChangeNodeStatus(completedStatus)
+		return nil
+	}
+
+	failedStatus, err := types.CreateNodeStatus("failed")
+	if err != nil {
+		return err
+	}
+
+	nodeToRun.ChangeNodeStatus(failedStatus)
+	nodeToRun.ChangeError("Error not js valid command")
+
+	return fmt.Errorf("Error not js valid command")
+}
+
+func (fe *FlowEngineCore) RunFlow(flow entities.Flow) error {
+	err := fe.changeFlowStatus(flow, "running")
+	if err != nil {
+		return err
+	}
+
+	nodeToRun, err := fe.selectNextNodeToRun(&flow)
+	if err != nil {
+		return err
+	}
+
+	err = flow.SetCurrentNode(nodeToRun.ID)
+	if err != nil {
+		return err
+	}
+
+	err = fe.execJSNodeOrBun(&nodeToRun)
+	if err != nil {
+		return err
+	}
+
+	err = fe.updateFlowState(flow)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
