6e35349a50c852031e42ef1831c64f7034444579
Luiz Ricardo <luizrgfelipe@gmail.com>
2025-12-30 19:40:06 -0300

    ✅ TEST: implement new tests to nodes maked in go lang

--- internal/tests/integration/flow-state_routes_tests/create_flow_to_run_test.go
+++ internal/tests/integration/flow-state_routes_tests/create_flow_to_run_test.go
@@ -5,13 +5,18 @@ import (
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
+	"os"
+	"path/filepath"
 	"testing"
+	"time"
 
 	"github.com/google/uuid"
 	"github.com/luizrgf2/universal-flow/internal/core/usecases"
 	"github.com/luizrgf2/universal-flow/internal/presentation"
 	"github.com/stretchr/testify/assert"
 )
+
+var baseUrl = "http://localhost:8080/api/flow-state"
 
 func createNodesToTest() []usecases.CreateFlowUseCaseNodes {
 
@@ -40,23 +45,133 @@ func createNodesToTest() []usecases.CreateFlowUseCaseNodes {
 
 }
 
+func createNodesInGoToTeste() []usecases.CreateFlowUseCaseNodes {
+	startID := uuid.NewString()
+	midID := uuid.NewString()
+	endID := uuid.NewString()
+
+	return []usecases.CreateFlowUseCaseNodes{
+		{
+			ID:         startID,
+			Name:       "start",
+			ScriptPath: "go run /home/luizrgf/projetos/meus/universal-flow/internal/tests/nodes_go_to_test/nodeStart/nodeStartToTest.go",
+			OutputNode: []string{midID},
+		},
+		{
+			ID:         midID,
+			Name:       "mid",
+			ScriptPath: "go run /home/luizrgf/projetos/meus/universal-flow/internal/tests/nodes_go_to_test/nodeMid/nodeMidToTest.go",
+			OutputNode: []string{endID},
+		},
+		{
+			ID:         endID,
+			Name:       "end",
+			ScriptPath: "go run /home/luizrgf/projetos/meus/universal-flow/internal/tests/nodes_go_to_test/nodeEnd/nodeEndToTest.go",
+			OutputNode: []string{},
+		},
+	}
+}
+
+func createNodesInBunToTeste() []usecases.CreateFlowUseCaseNodes {
+	startID := uuid.NewString()
+	midID := uuid.NewString()
+	endID := uuid.NewString()
+
+	return []usecases.CreateFlowUseCaseNodes{
+		{
+			ID:         startID,
+			Name:       "start",
+			ScriptPath: "bun /home/luizrgf/projetos/meus/universal-flow/internal/tests/nodes_js_to_test/nodeStartToTest.ts",
+			OutputNode: []string{midID},
+		},
+		{
+			ID:         midID,
+			Name:       "mid",
+			ScriptPath: "bun /home/luizrgf/projetos/meus/universal-flow/internal/tests/nodes_js_to_test/nodeMidToTest.ts",
+			OutputNode: []string{endID},
+		},
+		{
+			ID:         endID,
+			Name:       "end",
+			ScriptPath: "bun /home/luizrgf/projetos/meus/universal-flow/internal/tests/nodes_js_to_test/nodeEndToTest.ts",
+			OutputNode: []string{},
+		},
+	}
+}
+
+func waitForFileAndCleanUp(t *testing.T, path string) {
+	t.Helper()
+	timeout := time.After(5 * time.Second)
+	ticker := time.NewTicker(250 * time.Millisecond)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-timeout:
+			t.Fatalf("timed out after 5 seconds waiting for file %s", path)
+		case <-ticker.C:
+			_, err := os.Stat(path)
+			if err == nil {
+				os.Remove(path) // Clean up the file
+				return          // File found, exit successfully
+			} else if !os.IsNotExist(err) {
+				t.Fatalf("error checking for file %s: %v", path, err)
+			}
+			// If file does not exist, continue loop
+		}
+	}
+}
+
 func TestToCreateFlowToRun(t *testing.T) {
 	route := presentation.StartServer()
-	w := httptest.NewRecorder()
+	server := httptest.NewServer(route)
+	defer server.Close()
 
-	nodesToBody := createNodesToTest()
+	nodesToBody := createNodesInGoToTeste()
 
 	body := usecases.CreateFlowUseCaseInput{
 		ID:    uuid.NewString(),
 		Nodes: nodesToBody,
-		Name:  "flowTest",
+		Name:  "flowTestGo",
 	}
 
 	bodyJson, _ := json.Marshal(body)
 
-	req, _ := http.NewRequest("POST", "/api/flow-state/create-flow-to-run", bytes.NewBuffer(bodyJson))
-	route.ServeHTTP(w, req)
+	resp, err := http.Post(baseUrl+"/create-flow-to-run", "application/json", bytes.NewBuffer(bodyJson))
+	assert.NoError(t, err)
 
-	assert.Equal(t, 200, w.Code)
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
 
+	dir, err := os.Getwd()
+	assert.NoError(t, err)
+	pathOfFile := filepath.Join(dir, "..", "..", "..", "..", "nodeEndToTestOutput.json")
+
+	waitForFileAndCleanUp(t, pathOfFile)
+}
+
+func TestToCreateFlowWithBunNodes(t *testing.T) {
+	route := presentation.StartServer()
+	server := httptest.NewServer(route)
+	defer server.Close()
+
+	nodesToBody := createNodesInBunToTeste()
+
+	body := usecases.CreateFlowUseCaseInput{
+		ID:    uuid.NewString(),
+		Nodes: nodesToBody,
+		Name:  "flowTestBun",
+	}
+
+	bodyJson, _ := json.Marshal(body)
+
+	resp, err := http.Post(baseUrl+"/create-flow-to-run", "application/json", bytes.NewBuffer(bodyJson))
+	assert.NoError(t, err)
+
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+
+	dir, err := os.Getwd()
+	assert.NoError(t, err)
+	pathOfFile := filepath.Join(dir, "..", "..", "..", "..", "nodeEndToTestOutput.json")
+
+	waitForFileAndCleanUp(t, pathOfFile)
 }
